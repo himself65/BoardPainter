86d1f698bed523957ef5d082771e70e574c779aa
himself65 <761282619@qq.com>
2017-12-30 21:59:46 +0800

    项目提交

---
+++ __main__.py
@@ -0,0 +1,61 @@
+import ssl
+import time
+
+from PIL import Image
+
+import luogu
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+url = "https://www.luogu.org/paintBoard"
+posturl = "https://www.luogu.org/paintBoard/paint"
+boaedurl = "https://www.luogu.org/paintBoard/board"
+
+header = {}
+header['user-agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+header['accept-encoding'] = "gzip, deflate, br"
+header['accept-language'] = "zh,en;q=0.9,zh-CN;q=0.8,ja;q=0.7"
+
+# cookie
+header['cookie'] = luogu.get_cookie()
+opener = luogu.getOpener(header)
+
+if __name__:
+    print("注意:\n时常更换cookie")
+    print("当前cookie: " + header['cookie'])
+    ###
+    im = Image.open("newhead.jpg")
+    w,h = im.size
+    count = 0
+    failtime = 0
+    data = open("data.json","r+")
+    data = data.read()
+    cookie,x,y,height,width = luogu.get_data(data)
+    for i in range(height,h+1,1):
+        for j in range(width,w+1,1):
+            color = luogu.get_color(im.getpixel((i,j)))
+            postdata = luogu.paintData(x,y,color)
+            op = opener.open(posturl,postdata)
+            data = op.read()
+            while luogu.check(data) == True:
+                count += 1
+                print("成功提交 "+ str(count) + "次")
+                
+                break
+            else:
+                time.sleep(1) # 失败时多等待一秒
+                op.opener(posturl,postdata)
+                data = op.read()
+                failtime += 1
+                print("失败提交 "+ str(failtime) + "次")
+            y += 1
+            luogu.save(height,width+1,x,y,cookie) # width得手动+1
+            time.sleep(30)
+        x += 1
+    
+    print("----------------")
+    print("完成提交")
+    print("总共提交:" + str(failtime+count))
+    print("其中")
+    print("成功提交 "+ str(count) + "次")
+    print("失败提交 "+ str(failtime) + "次")
---
+++ luogu.py
@@ -0,0 +1,161 @@
+import gzip
+import http.cookiejar
+import os
+import ssl
+import time
+import json
+from urllib import parse, request
+from colorsys import rgb_to_hsv
+
+fileLocation = "data.json"
+imageLocation = "newhead.jpg"
+colors = {
+    (0, 0, 0):0,
+    (255, 255, 255):1,
+    (170, 170, 170):2,
+    (85, 85, 85):3,
+    (254, 211, 199):4,
+    (255, 196, 206):5,
+    (250, 172, 142):6,
+    (255, 139, 131):7,
+    (244, 67, 54):8,
+    (233, 30, 99):9,
+    (226, 102, 158):10,
+    (156, 39, 176):11,
+    (103, 58, 183):12,
+    (63, 81, 181):13,
+    (0, 70, 112):14,
+    (5, 113, 151):15,
+    (33, 150, 243):16,
+    (0, 188, 212):17,
+    (59, 229, 219):18,
+    (151, 253, 220):19,
+    (22, 115, 0):20,
+    (55, 169, 60):21,
+    (137, 230, 66):22,
+    (215, 255, 7):23,
+    (255, 246, 209):24,
+    (248, 203, 140):25,
+    (255, 235, 59):26,
+    (255, 193, 7):27,
+    (255, 152, 0):28,
+    (255, 87, 34):29,
+    (184, 63, 39):30,
+    (121, 85, 72):31,
+}
+
+def get_color(pixel):
+    return min_color_diff(pixel, colors)
+
+def ungzip(data):
+    try:
+        print("正在解压")
+        data = gzip.decompress(data)
+        print("解压完成")
+    except:
+        print("解压失败")
+    return data
+
+def getOpener(head):
+    cj = http.cookiejar.CookieJar()
+    pro = request.HTTPCookieProcessor(cj)
+    opener = request.build_opener(pro)
+    header = []
+    for key, value in head.items():
+        elem = (key, value)
+        header.append(elem)
+    opener.addheaders = header
+    return opener
+
+def paintData(x, y, color):
+    postDict = {
+        'x' : str(x),
+        'y' : str(y),
+        'color' : str(color)
+    }
+    postData = parse.urlencode(postDict).encode()
+    return postData
+
+# 接受op.read内容
+def check(data):
+    data = json.loads(data)
+    if (data['status'] == 200):
+        return True
+    else:
+        return False
+
+def paintOnLine(x,y,color):
+    """ 此函数已经废弃，本意是画直线 """
+    postData = paintData(x,y,color)
+    op = opener.open(posturl,postData)
+    while check(op.read()) != True:
+        postData = paintData(x,y,color)
+        op = opener.open(posturl,postData)
+
+def paintline(x0,y0,x1,y1,color): # 实现自动化画线？？
+    """ 此函数已经废弃，本意是画直线 """
+    dx = abs(x1-x0)
+    dy = abs(y1-y0)
+    if x0 < x1:
+        sx = 1
+    else:
+        sx = -1
+    if y0 < y1:
+        sy = 1
+    else:
+        sy = -1
+    if dx > dy:
+        err = dx
+    else:
+        err = -dy
+    err = err / 2
+    paintOnLine(x0,y0,color)
+    while (x0 != x1 and y0 != y1):
+        e2 = err
+        if e2 > -dx:
+            err = err - dy
+            x0 = x0 + sx
+        if e2 < dy:
+            err = err + dx
+            y0 = y0 + sy
+        time.sleep(16) # 此处等待16秒，多等一会儿
+        paintOnLine(x0,y0,color)
+
+def to_hsv( color ): 
+    return rgb_to_hsv(*[x/255.0 for x in color])
+
+def color_dist( c1, c2):
+    return sum( (a-b)**2 for a,b in zip(to_hsv(c1),to_hsv(c2)) )
+
+def min_color_diff( color_to_match, colors):
+    """ 返回字典中最小颜色差值的值"""
+    return min(
+        (color_dist(color_to_match, test), colors[test])
+        for test in colors)
+
+def get_data(data):
+    data = json.loads(data)
+    return data["cookie"],data["x"],data["y"],data["height"],data["width"]
+
+def save(height,width,x,y,cookie):
+    """ 保存数据到本地 """
+    data = {
+        "cookie":cookie,
+        "x":x,
+        "y":y,
+        "height":height,
+        "width":width
+    }
+    data = json.dumps(data)
+    try:
+        f = open(fileLocation,'w+')
+        f.write(data)
+    except:
+        print("储存错误")
+    finally:
+        f.close()
+    
+def get_cookie():
+    f = open(fileLocation,'r')
+    data = json.load(f)
+    return data["cookie"]
