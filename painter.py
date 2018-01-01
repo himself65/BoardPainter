f54d01ff1cacbada0807bdb441171a378477e949
himself65 <761282619@qq.com>
2018-01-01 23:24:05 +0800

    cookie时常更换，改用js提交

--- painter.py
+++ painter.py
@@ -13,10 +13,10 @@ header = {}
 header['user-agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
 header['accept-encoding'] = "gzip, deflate, br"
 header['accept-language'] = "zh,en;q=0.9,zh-CN;q=0.8,ja;q=0.7"
-old_header = header
 # cookie
 header['cookie'] = luogu.get_cookie()
 opener = luogu.getOpener(header)
+
 if __name__:
     ssl._create_default_https_context = ssl._create_unverified_context
     print("注意:\n时常更换cookie")
@@ -26,36 +26,23 @@ if __name__:
     w,h = im.size
     count = 0
     failtime = 0
-    data = open("data.json","r+")
+    data = open("data.json","r")
     data = data.read()
-    cookie,x,y,height,width = luogu.get_data(data)
-    for i in range(height,h+1,1):
-        for j in range(width,w+1,1):
+    cookie,px,py,height,width = luogu.get_data(data)
+    
+    print("开始提交")
+    for i in range(height,h,1):
+        x = px + i    
+        for j in range(width,w,1):
+            y = py + j
             color = luogu.get_color(im.getpixel((i,j)))
             postdata = luogu.paintData(x,y,color)
             op = opener.open(posturl,postdata)
-            print(cookie)
             data = op.read()
-            while luogu.check(data) == True:
-                count += 1
-                print("成功提交 "+ str(count) + "次")
-                
-                break
-            else:
-                time.sleep(1) # 失败时多等待一秒
-                op.opener(posturl,postdata)
-                data = op.read()
-                failtime += 1
-                print("失败提交 "+ str(failtime) + "次")
-            y += 1
-
-            luogu.save(i,j+1,x,y,cookie) # width得手动+1
-            time.sleep(30)
-        x += 1
-    
+            count += 1
+            print("成功提交 "+ str(count) + "次")
+            luogu.save(i,j,px,py,cookie)
+            print(i,j,x,y,color)
+            time.sleep(30) # 等待时间
     print("----------------")
     print("完成提交")
-    print("总共提交:" + str(failtime+count))
-    print("其中")
-    print("成功提交 "+ str(count) + "次")
-    print("失败提交 "+ str(failtime) + "次")
